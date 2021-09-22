575f6303404de8b1775fb30229cbf806730931e3
pillaiuma <88091566+pillaiuma@users.noreply.github.com>
2021-09-23 01:30:05 +0530

    Add files via upload

---
+++ Terraform/Asg24_2/variables.tf
@@ -0,0 +1,18 @@
+variable "aws_region" {
+  default = "us-east-1"
+}
+
+variable "project_name" {
+  default = "la-terraform"
+}
+
+variable "number_of_instances" {
+  default=2
+}
+
+variable "access_key" {
+  default=""
+}
+variable "secret_key" {
+  default=""
+}
